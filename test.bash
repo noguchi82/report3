e3b2f8fa9489d06fa8305b3c65ac8ef952024734
noguchi82 <s20C1093JP@s.chibakoudai.jp>
2023-01-23 20:25:18 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -9,19 +9,19 @@ ng () {
 
 res=0
 
-### I/O TEST ###
+### I/0 TEST ###
 out=$(echo 3 |./result)
-[ "${out}"= 奇数 ]  || ng $LINENO 
+[ "${out}"= 奇数 ]  || ng ${LINENO}
 
 
 ###STRANGE INPUT ###
 out=$(echo あ | ./result)
-[ "$?" = 1 ]        || ng $LINENO
-[ "${out}" = "" ]   || ng $LINENO
+[ "$?" = 1 ]        || ng ${LINENO}
+[ "${out}" = "" ]   || ng ${LINENO}
 
 out=$(echo | ./result)
-[ "$?" = 1 ]        || ng $LINENO
-[ "${out}" = "" ]   || ng $LINENO
+[ "$?" = 1 ]        || ng ${LINENO}
+[ "${out}" = "" ]   || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
